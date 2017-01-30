bbc3a354ea1faed66b65e14d1ebeebf7f10b3b47
Muhammad Ammar Qureshi <qureshm@tcd.ie>
2017-01-30 22:59:13 +0000

    first commit

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class App {
+
+	
+	public static int Add(int x, int y){
+		return x + y;
+	}
+}
---
+++ src/test/java/com/test/Apptest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		assertEquals(8,App.Add(4,4));
+	}
+
+}
